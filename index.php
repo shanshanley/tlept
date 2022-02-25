a3fec213e797ab8a1cce0ce7bb638acb18d6ca7e
shanshanley <98719942+shanshanley@users.noreply.github.com>
2022-02-26 03:14:44 +0800

    Update and rename README.md to index.php

---
+++ index.php
@@ -0,0 +1,80 @@
+<html>
+<table border="0" width="100%" cellpadding="0" cellspacing="0" bgcolor="#03EE5C"   >
+<tr>
+<td>
+<table border="0 " width="85%" cellpadding="15" cellspacing="0" align="center">
+<tr>
+<td>
+<font face="serif" color="#050000" size="6">
+Group 14 
+</font> 
+</td>
+<td width="30%">&nbsp;</td>
+<td> <font face="arial" color="#0E6500" size="3">
+Heart Shanley <br> Sasota
+</font>  </td>
+<td> <font face="arial" color="#0E6500" size="3">
+Margarette Antonette <br> Mahusay
+</font>  </td>
+<td> <font face="arial" color="#0E6500" size="3">
+Justine <br> Caytona
+</font> </td>
+<td> <font face="arial" color="#0E6500" size="3">
+Alfred <br> Rosales
+</font>  </td> 
+</tr> </table>
+</td>
+</tr>
+</table>
+
+<table border="0" width="100%" cellpadding="0" cellspacing="0" bgcolor="#444444">
+<tr> <td> 
+<table border="0" width="85%" cellpadding="15" cellspacing="15" align="center">
+<tr> <td align="center" valign="middle" height="300"> 
+<h3> <marquee behavior="alternate" direction="left" scrollamount="2"> 
+<font face="arial" color="#ffffff" size="5">
+Hello! This is Group 14's Performance Tasks
+</marquee> </font> </h3>
+<h1> <marquee behavior="alternate" direction="right" scrollamount="2">  
+<font face="arial" color="#03EE5C" size="7">
+Subjects: CAE, MATH, and CLE 
+</marquee> </font> </h1>
+</td> </tr> <table> </td> </tr> </table>
+
+<table border="0" width="100%" cellpadding="0" cellspacing="0" bgcolor="#292929">
+<tr> <td> 
+<table border="0" width="85%" cellpadding="15" cellspacing="0" align="center">
+<tr> <td height="160" align="center" valign="middle" colspan="3">
+<font fance="arial" size="6" color="#03EE5C"> Subjects </font>
+<hr width="70" color="#03EE5C"> </tr> </td>
+
+<td width="33.33%" valign="top">
+<table border="0" width="100%" cellpadding="15" cellspacing="0" align="center" bgcolor="#353535">
+<tr> <td> 
+<font face="arial" size="5" color="#ffffff">
+<center> CAE </font> 
+<br> <br>
+<a href="https://ptcae.herokuapp.com/"> <font face="arial" size="4" color="#03EE5C"> 
+Click here to access the PT </center> </font> <br> 
+</td> </tr> </table> </td>
+
+<td width="33.33%" valign="top">
+<table border="0" width="100%" cellpadding="15" cellspacing="0" align="center" bgcolor="#353535">
+<tr> <td> 
+<font face="arial" size="5" color="#ffffff">
+<center> MATH </font> 
+<br> <br>
+<a href="https://mathematicspt.herokuapp.com/"> <font face="arial" size="4" color="#03EE5C"> 
+Click here to access the PT </center> </font> <br>
+</td> </tr> </table> </td>
+
+<td width="33.33%" valign="top">
+<table border="0" width="100%" cellpadding="15" cellspacing="0" align="center" bgcolor="#353535">
+<tr> <td> 
+<font face="arial" size="5" color="#ffffff">
+<center> CLE </font> 
+<br> <br>
+<a href="https://ptcle.herokuapp.com/"> <font face="arial" size="4" color="#03EE5C"> 
+Click here to access the PT </center> </font> <br> 
+</td> </tr> </table> </td>
+</html>
